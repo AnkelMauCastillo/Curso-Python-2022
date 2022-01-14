59798b7aad4ee78ad7932669ed2a68b97d898c5b
AnkelMauCastillo <angel.castillo.zavala@estudiante.uacm.edu.mx>
2022-01-13 20:37:03 -0600

    Create ejemplo-funciones.py

---
+++ introducción/ejemplo-funciones.py
@@ -0,0 +1,27 @@
+from msilib.schema import Error
+import re
+
+
+def suma (a, b):
+    return a + b
+
+def resta (a, b):
+    return a - b
+
+def multiplicacion (a, b):
+    return a * b
+
+def division (a, b):
+    if b == 0:
+        return 'no esta definido'
+    
+    return a / b           
+
+
+valorA = int(input ('Ingrese el valor a: '))
+valorB = int(input ('Ingrese el valor b: '))
+
+print (f'La suma de {valorA} y {valorB} es : {suma(valorA, valorB)}')
+print (f'La resta de {valorA} y {valorB} es : {resta(valorA, valorB)}')
+print (f'La division de {valorA} y {valorB} es : {division(valorA, valorB)}')
+print (f'La multiplicacion de {valorA} y {valorB} es : {multiplicacion(valorA, valorB)}')
