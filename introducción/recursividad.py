6db80bade0636c7cd6639a7f406378163d968542
AnkelMauCastillo <angel.castillo.zavala@estudiante.uacm.edu.mx>
2022-01-14 17:20:26 -0600

    Update recursividad.py

--- introducción/recursividad.py
+++ introducción/recursividad.py
@@ -3,5 +3,11 @@ def factorial (numero):
         return 1
     return numero * factorial(numero - 1)
 
+def fibonacci (numero):
+    if numero == 0 or numero == 1 :
+        return 1
+    return fibonacci (numero - 1) + fibonacci (numero - 2)   
+
 numFactorial = int( input ("Ingresa un valor positivo: ")) 
 print (f'El factorial de {numFactorial} es: {factorial(numFactorial)}')
+print (f'El fibonacci de {numFactorial} es: {fibonacci(numFactorial)}')
