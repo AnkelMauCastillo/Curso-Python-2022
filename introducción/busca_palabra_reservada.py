4db860221261d1dea3e09920d7d34e38bb9c6110
AnkelMauCastillo <angel.castillo.zavala@estudiante.uacm.edu.mx>
2022-01-14 21:13:24 -0600

    Create busca_palabra_reservada.py

---
+++ introducción/busca_palabra_reservada.py
@@ -0,0 +1,11 @@
+palabrasReservadas = ["if", "else", "let", "endif", "endlet"]
+
+esPalabra = str(input("Dame una palabra Reservada:"))
+
+if esPalabra in palabrasReservadas :
+  print("Si es una palabra Reservada")
+else :
+  print("No es palabra Reservada") 
+
+
+
